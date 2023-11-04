7553964d0b3d085ef5b0c329c284adcd1aa88dd3
jeremysr <jeremysr@protonmail.com>
2023-11-04 14:02:55 -0600

    added about section with placeholder, will be edited later

---
+++ portfolio/src/components/about/About.jsx
@@ -0,0 +1,17 @@
+import './About.css'
+
+export default function About() {
+    return (
+      <>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+      </>
+    )
+  }
