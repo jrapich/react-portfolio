232029c2978b0f7b3fc5f527c7e25adc5aed56e0
jeremysr <jeremysr@protonmail.com>
2023-11-05 16:35:50 -0700

    added projects state, we will render an array of current projects on page load and pass them as props to Porfolio component

--- portfolio/src/components/pages/portfolio/Portfolio.jsx
+++ portfolio/src/components/pages/portfolio/Portfolio.jsx
@@ -1,8 +1,31 @@
+import {useState, useEffect} from 'react';
+import Project from '../project/Project';
+//import project data constructor
+import ProjectObj from '../../../utils/projects';
+
 
 export default function Portfolio(){
+    //state for each project on the portfolio page
+    const [projects, setProjects] = useState([]);
+
+    //array of objects that contain my project info
+    //each object will render to the portfolio page as its own project card
+    const projectData = [];
+    projectData.push(new ProjectObj(1,'name','github link','screenshot'));
+    projectData.push(new ProjectObj(2,'name','github link','screenshot'));
+    projectData.push(new ProjectObj(3,'name','github link','screenshot'));
+    projectData.push(new ProjectObj(4,'name','github link','screenshot'));
+    projectData.push(new ProjectObj(5,'name','github link','screenshot'));
+    projectData.push(new ProjectObj(6,'name','github link','screenshot'));
+
+    //useEffect to render all of the above projects in the portfolio on page load
+    useEffect(()=>{
+        setProjects(projectData);
+    }, []);
+
     return(
         <div>
-            palceholder
+            <Project projects={projects} />
         </div>
     );
 }
