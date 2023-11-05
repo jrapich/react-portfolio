25c86717a53a57ddc5927130fdec289ff59dd9c7
jeremysr <jeremysr@protonmail.com>
2023-11-05 16:30:41 -0700

    changed constructor obj slightly

--- portfolio/src/utils/projects.js
+++ portfolio/src/utils/projects.js
@@ -1,10 +1,9 @@
 //project constructor to build a new object containing project data to display on the portfolio
 export default class ProjectObj {
-    constructor(id, name, link, image, description){
+    constructor(id, name, link, image){
         this.id = id;
         this.projectName = name;
         this.link = link;
         this.image = image;
-        this.description = description;
     }
 };
