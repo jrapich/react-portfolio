379cfdf6f5d037cccffb9dfd6a1f29c0feab8d02
jeremysr <jeremysr@protonmail.com>
2023-11-05 15:24:38 -0700

    added placeholder footer we will render on each page

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Header from './components/UI/header/Header'
 import { Outlet } from 'react-router-dom' 
+import Footer from './components/UI/footer/Footer'
 
 function App() {
   return (
     <>
     <Header />
     <Outlet />
+    <Footer />
     </>
   );
 }
---
+++ portfolio/src/components/UI/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export default function Footer() {
+    return(
+        <footer className='row container-fluid bg-body-secondary text-light justify-content-center' data-bs-theme="dark">
+            Footer goes here
+        </footer>
+    );
+}
