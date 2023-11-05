31bc7e5b4e35cdbf9a88873c6270b10841a6bcc8
jeremysr <jeremysr@protonmail.com>
2023-11-05 16:19:15 -0700

    added constructor class for project data of the portfolio

---
+++ portfolio/src/utils/projects.js
@@ -0,0 +1,10 @@
+//project constructor to build a new object containing project data to display on the portfolio
+export default class Project {
+    constructor(id, name, link, image, description){
+        this.id = id;
+        this.projectName = name;
+        this.link = link;
+        this.image = image;
+        this.description = description;
+    }
+};
