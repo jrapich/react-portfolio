d01deee23fa1f5c9e459588c969b1cc6cb1beeda
jeremysr <jeremysr@protonmail.com>
2023-11-05 17:36:03 -0700

    render each project in the portfolio by mapping the array of projects passed as a prop

--- portfolio/src/components/pages/project/Project.jsx
+++ portfolio/src/components/pages/project/Project.jsx
@@ -1,6 +1,22 @@
 
-export default function Project () {
+export default function Project (props) {
     return (
-        <div>placeholder</div>
+        <section className='row container-fluid' id="projects">
+            <div className="col-2"></div>
+            <section className="col container-fluid">
+                {props.projects.map((project)=>(
+                    <div className="card" key={project.id}>
+                    <a href={project.link}>
+                        <img src={project.image} className="card-img-top" alt=""></img>
+                    </a>
+                    <div className="card-body">
+                        {console.log(project.name)}
+                        <p className="card-text">{project.projectName}</p>
+                    </div>
+                </div>
+                ))}
+            </section>
+            <div className="col-2"></div>
+        </section>
     )
 };
