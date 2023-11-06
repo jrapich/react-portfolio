de4123a6809068aae7b3c4e86b18231db21db64f
jeremysr <jeremysr@protonmail.com>
2023-11-05 21:45:44 -0700

    added deployed links to some applications//added descriptions to projects

--- portfolio/src/utils/projects.js
+++ portfolio/src/utils/projects.js
@@ -24,6 +24,14 @@ const currentProjects = {
         'Simple SVG Generator',
         'README Generator'],
     link:[
+        'https://lacks-production.up.railway.app/',
+        'https://tech-blog-production-fedd.up.railway.app/',
+        'https://github.com/jrapich/social-media-API',
+        'https://newtron21.github.io/Starmapper/',
+        'https://github.com/jrapich/SVG-generator',
+        'https://github.com/jrapich/readme-generator'
+    ],
+    github:[
         'https://github.com/daestanii1997/lacks',
         'https://github.com/jrapich/tech-blog',
         'https://github.com/jrapich/social-media-API',
@@ -32,6 +40,24 @@ const currentProjects = {
         'https://github.com/jrapich/readme-generator'
     ],
     image:[lacks,techblog,social,starmapperJWST,svg,readme],
+    description:[
+        `Lacks, a fullstack app, is a simple, and humorous Slack clone, built as a group project with 4 people.
+        It utilizes the MVC model. 
+        Not entirely finished and could use some work. It was built using ExpressJS and SQL on the backend, with Handlebars rendering the frontend. `,
+
+        `A simple CMA style tech blog fullstack app. Built with Express, Handlebars and SQL, following the MVC model.
+        Complete with a working front end, user auth and login via SQL, session saving via cookie, and a functioning blog/comments system,
+        this blog is my proudest work to date with the sheer amount of work it took to get it all working.`,
+
+        'A RESTful back end API for a mock social media website, using mongodb and mongoose to store/retrieve data, with Express providing the API routes.',
+
+        `STARMAPPER. Then result of our first group project of the course, it interacts with two seperate remote APIs to display a constellation in the night sky based
+        on the city the user searches with.`,
+
+        `A backend app using Node and Javascript to generate very simple shapes via SVG file.`,
+
+        `A backend app using Node and Javascript to quickly generate markdown to use for a README.md file. Also generates badges that can then be added to the readme.`
+    ],
 
 }
 
