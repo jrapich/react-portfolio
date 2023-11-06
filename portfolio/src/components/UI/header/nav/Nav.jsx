19fd42ccdaca75d40274442d43be893c0dbedc63
jeremysr <jeremysr@protonmail.com>
2023-11-05 22:20:54 -0700

    sizing and responsiveness edits

--- portfolio/src/components/UI/header/nav/Nav.jsx
+++ portfolio/src/components/UI/header/nav/Nav.jsx
@@ -1,20 +1,26 @@
-import me from '../../../../assets/images/me.jpg'
-import './Nav.css'
+import me from '../../../../assets/images/me.jpg';
+import pillars from '../../../../assets/images/pillars.png';
+import './Nav.css';
 
 export default function Nav({links}) {
     return(
         <nav className="navbar bg-body-secondary" data-bs-theme="dark">
             <div className="container-fluid  row justify-content-center">
-                <div className="col text-light">
-                    Jeremy Rapich
-                    <img src={me} width="300" height="300" className="d-inline-block align-text-top" id='navImage'></img>
-                </div>
-                <div className="col navbar-brand">
+                <section className="col-2 text-light">
+                    <div>
+                        <img src={me} width="200" height="200" className="d-inline-block align-text-top navImage"></img>
+                    </div>
+                </section>
+                <section className="col navbar-brand">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         {links.map((link) => link)}
                     </ul>
-                </div>
-                <div className="col">add pillars of creation here</div>
+                </section>
+                <section className="col-2">
+                    <div>
+                        <img src={pillars} width="200" height="200" className="d-inline-block align-text-top navImage"></img>
+                    </div>
+                </section>
             </div>
         </nav>
     );
