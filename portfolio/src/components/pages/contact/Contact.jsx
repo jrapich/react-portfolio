4ea2def45577c655ea6b0d720f3bd28f17f4c452
jeremysr <jeremysr@protonmail.com>
2023-11-05 19:14:49 -0700

    added simple contact info

--- portfolio/src/components/pages/contact/Contact.jsx
+++ portfolio/src/components/pages/contact/Contact.jsx
@@ -1,6 +1,11 @@
 
 export default function Contact () {
     return (
-        <div>placeholder</div>
+        <section id="contactMe">
+            <h3>Email: jeremysr@proton.me</h3>
+            <h3>Phone/Signal Messenger/SMS: 435-840-8800</h3>
+            <h3>Matrix: @exist:monero.social</h3>
+            <h3>PGP: C2054C18</h3>
+        </section>
     )
 };
