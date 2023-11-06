31a78b56588f4a6b4d6a5ae99ffe651d0645d6a6
jeremysr <jeremysr@protonmail.com>
2023-11-05 21:56:35 -0700

    added github link and project description to components

--- portfolio/src/components/pages/portfolio/Portfolio.jsx
+++ portfolio/src/components/pages/portfolio/Portfolio.jsx
@@ -2,7 +2,7 @@ import {useState, useEffect} from 'react';
 import Project from '../project/Project';
 //import project data constructor
 import {ProjectObj, currentProjects} from '../../../utils/projects';
-const {projectName, link, image} = currentProjects;
+const {projectName, link, github, image, description} = currentProjects;
 
 export default function Portfolio(){
     //state for each project on the portfolio page
@@ -12,7 +12,7 @@ export default function Portfolio(){
     //each object will render to the portfolio page as its own project card
     const projectData = [];
     for (let i = 0; i < link.length; i++) {
-        projectData.push(new ProjectObj(i+1, projectName[i], link[i], image[i]));
+        projectData.push(new ProjectObj(i+1, projectName[i], link[i], github[i], image[i], description[i]));
     }
 
     //useEffect to render all of the above projects in the portfolio on page load
--- portfolio/src/components/pages/project/Project.jsx
+++ portfolio/src/components/pages/project/Project.jsx
@@ -5,13 +5,16 @@ export default function Project (props) {
         <section className='row container-fluid' id="projects">
             <div className="col-2"></div>
             <section className="col container-fluid">
+                <h4>Here are a list of apps I have developed. Feel free to click on a image to be taken to the deployed app's site, if it has one.</h4>
                 {props.projects.map((project)=>(
                     <div className="card" key={project.id}>
                     <a href={project.link}>
                         <img src={project.image} className="card-img-top" alt="screenshot"></img>
                     </a>
                     <div className="card-body">
-                        <p className="card-text">{project.projectName}</p>
+                        <h4 className="card-text">{project.projectName}</h4>
+                        <p className='card-text'>{project.description}</p>
+                        <a href={project.github}>Github Repo</a>
                     </div>
                 </div>
                 ))}
