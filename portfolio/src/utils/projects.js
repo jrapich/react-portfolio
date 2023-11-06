abdf62124d42ba1086b445880f8f31971a955273
jeremysr <jeremysr@protonmail.com>
2023-11-05 21:03:44 -0700

    typo fixes

--- portfolio/src/utils/projects.js
+++ portfolio/src/utils/projects.js
@@ -1,12 +1,12 @@
 //project constructor to build a new object containing project data to display on the portfolio
 import lacks from '../assets/images/lacks.png';
-import techblog from '../assets/images/tech0blog.png';
+import techblog from '../assets/images/tech-blog.png';
 import social from '../assets/images/social-api.png';
 import starmapperJWST from '../assets/images/main_image_star-forming_region_carina_nircam_final-5mb.jpg';
-import svg from  '../assets/image/svg.png';
+import svg from  '../assets/images/svg.png';
 import readme from '../assets/images/readme.png';
 
-export class ProjectObj {
+class ProjectObj {
     constructor(id, name, link, image){
         this.id = id;
         this.projectName = name;
@@ -15,7 +15,7 @@ export class ProjectObj {
     }
 };
 
-export const currentProjects = {
+const currentProjects = {
     projectName:[
         'Lacks(Slack Clone)',
         'Tech-Blog',
@@ -34,3 +34,5 @@ export const currentProjects = {
     image:[lacks,techblog,social,starmapperJWST,svg,readme],
 
 }
+
+export {ProjectObj, currentProjects};
