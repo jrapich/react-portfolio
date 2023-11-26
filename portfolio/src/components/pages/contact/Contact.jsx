f9006e54236bad7e0bd3b36fd7da6478c4a33558
jeremysr <jeremysr@protonmail.com>
2023-11-25 17:36:36 -0700

    converted js queryselectors to use react refs

--- portfolio/src/components/pages/contact/Contact.jsx
+++ portfolio/src/components/pages/contact/Contact.jsx
@@ -1,53 +1,56 @@
+import { useRef } from 'react';
+
 const emailRegex = new RegExp(/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/gm);
 
-//function to change the border of a field to blue when mouse hovers over it
-const handleFieldMouseEnter = (event) => {
-    event.target.classList.add("border-primary");
-}
-
-//series of logic to check against a form field when the mouse exits
-//will check to see if the field is empty, or an incorrect email address, then show a red error message if it is
-const handleFieldMouseLeave = (event) => {
-    const nameHelp = document.querySelector('#nameHelp');
-    const emailHelp = document.querySelector('#emailHelp');
-    const passwordHelp = document.querySelector('#passwordHelp');
-    const yourName = document.querySelector('#yourName').value();
-    const yourEmail = document.querySelector('#yourEmail').value();
-    const yourPassword = document.querySelector('#yourPassword').value();
-
-    if (!yourName) {
-        event.target.classList.add("border-danger");
-        nameHelp.textContent = "This field is required.";
-        nameHelp.classList.add(".text-danger");
-    } else {
-        nameHelp.textContent = 'Enter your name here.';
-        nameHelp.classList.remove(".text-danger");
-    }
-
-    if (!yourEmail) {
-        event.target.classList.add("border-danger");
-        emailHelp.textContent = "This field is required.";
-        emailHelp.classList.add(".text-danger");
-    } else if (!emailRegex.test(yourEmail)) {
-        event.target.classList.add("border-danger");
-        emailHelp.textContent = "Please enter a valid email address.";
-        emailHelp.classList.add(".text-danger");
-    } else {
-        emailHelp.textContent = 'Enter your email here.';
-        emailHelp.classList.remove(".text-danger");
-    }
-
-    if (!yourPassword) {
-        event.target.classList.add("border-danger");
-        passwordHelp.textContent = "This field is required.";
-        passwordHelp.classList.add(".text-danger");
-    } else {
-        passwordHelp.textContent = 'Enter your password here.';
-        passwordHelp.classList.remove(".text-danger");
-    }
-}
-
 export default function Contact () {
+    const nameRef = useRef(null);
+    const nameHelpRef = useRef(null);
+    const emailRef = useRef(null);
+    const emailHelpRef = useRef(null);
+    const passwordRef = useRef(null);
+    const passwordHelpRef = useRef(null);
+
+    //function to change the border of a field to blue when mouse hovers over it
+    const handleFieldMouseEnter = (event) => {
+        event.target.classList.add("border-primary");
+    }
+
+    //series of logic to check against a form field when the mouse exits
+    //will check to see if the field is empty, or an incorrect email address, then show a red error message if it is
+    const handleFieldMouseLeave = (event) => {
+        
+        if (nameRef.current.value === "") {
+            event.target.classList.add("border-danger");
+            nameHelpRef.current.innerText = "This field is required.";
+            nameHelpRef.current.classList.add(".text-danger");
+        } else {
+            nameHelpRef.current.innerText = 'Enter your name here.';
+            nameHelpRef.current.classList.remove(".text-danger");
+        }
+
+        if (emailRef.current.value === "") {
+            event.target.classList.add("border-danger");
+            emailHelpRef.current.innerText = "This field is required.";
+            emailHelpRef.current.classList.add(".text-danger");
+        } else if (!emailRegex.test(emailRef.current.value)) {
+            event.target.classList.add("border-danger");
+            emailHelpRef.current.innerText = "Please enter a valid email address.";
+            emailHelpRef.current.classList.add(".text-danger");
+        } else {
+            emailHelpRef.current.innerText = 'Enter your email here.';
+            emailHelpRef.current.classList.remove(".text-danger");
+        }
+
+        if (passwordRef.current.value === "") {
+            event.target.classList.add("border-danger");
+            passwordHelpRef.current.innerText = "This field is required.";
+            passwordHelpRef.current.classList.add(".text-danger");
+        } else {
+            passwordHelpRef.current.innerText = 'Enter your password here.';
+            passwordHelpRef.current.classList.remove(".text-danger");
+        }
+    }
+
     return (
         <div className="row container-fluid text-center">
             <div className="col-2 flex-grow-1"></div>
@@ -57,24 +60,42 @@ export default function Contact () {
                     <form>
                         <div className="mb-3">
                             <label htmlFor="yourName" className="form-label">Enter your name</label>
-                            <input 
+                            <input
+                                ref={nameRef}
                                 type="text" 
                                 className="form-control" 
                                 id="yourName" 
                                 aria-describedby="nameHelp"
                                 onMouseEnter={handleFieldMouseEnter}
-                                onMouseLeave={handleFieldMouseLeave}></input>
-                            <div id="nameHelp" className="form-text">Enter your name here.</div>
+                                onMouseLeave={handleFieldMouseLeave}
+                                ></input>
+                            <div ref={nameHelpRef} id="nameHelp" className="form-text">Enter your name here.</div>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="yourEmail" className="form-label">Email Address</label>
-                            <input type="email" className="form-control" id="yourEmail" aria-describedby="emailHelp"></input>
-                            <div id="emailHelp" className="form-text">Enter your email here</div>
+                            <input 
+                                ref={emailRef}
+                                type="email" 
+                                className="form-control" 
+                                id="yourEmail" 
+                                aria-describedby="emailHelp"
+                                onMouseEnter={handleFieldMouseEnter}
+                                onMouseLeave={handleFieldMouseLeave}
+                                ></input>
+                            <div ref={emailHelpRef} id="emailHelp" className="form-text">Enter your email here</div>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="yourPassword" className="form-label">Password</label>
-                            <input type="password" className="form-control" id="yourPassword" aria-describedby="passwordHelp"></input>
-                            <div id="passwordHelp" className="form-text">Enter your password here</div>
+                            <input 
+                                ref={passwordRef}
+                                type="password" 
+                                className="form-control" 
+                                id="yourPassword"
+                                aria-describedby="passwordHelp"
+                                onMouseEnter={handleFieldMouseEnter}
+                                onMouseLeave={handleFieldMouseLeave}
+                                ></input>
+                            <div ref={passwordHelpRef} id="passwordHelp" className="form-text">Enter your password here</div>
                         </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
