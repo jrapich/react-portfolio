c020b0f3e2b6bc1a3ac07c38fb5877936d787513
jeremysr <jeremysr@protonmail.com>
2023-11-25 18:26:40 -0700

    added github/twitter/linkedin links to footer

--- portfolio/src/components/UI/footer/Footer.jsx
+++ portfolio/src/components/UI/footer/Footer.jsx
@@ -2,8 +2,11 @@ import './Footer.css';
 
 export default function Footer() {
     return(
-        <footer className='row container-fluid bg-body-secondary text-light justify-content-center' data-bs-theme="dark">
-            &copy; Jeremy Rapich 2023
+        <footer className='row container-fluid bg-body-secondary text-light d-block' data-bs-theme="dark">
+            <span>&copy; Jeremy Rapich 2023 </span>
+            <span><a href='https://www.github.com/jrapich'>Github</a></span>
+            <span><a href='https://www.linkedin.com/in/jeremy-rapich' >LinkedIn</a></span>
+            <span><a href='https://www.twitter.com/jeremy_rapich' >Twitter</a></span>
         </footer>
     );
 }
