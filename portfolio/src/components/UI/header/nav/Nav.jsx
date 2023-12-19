40ef46c202d282d319d1355cd4658d4a9305c70c
jeremysr <jeremysr@protonmail.com>
2023-12-18 19:45:26 -0700

    edits to navbar so it takes up less space. added 2nd image on right side of navbar, added links to home to images

--- portfolio/src/components/UI/header/nav/Nav.jsx
+++ portfolio/src/components/UI/header/nav/Nav.jsx
@@ -1,13 +1,22 @@
 import me from '../../../../assets/images/me.jpg';
+import nightstreak from '../../../../assets/images/nightstreak-half.jpg'
 import './Nav.css';
 
 export default function Nav({links}) {
     return(
         <nav className="navbar bg-body-secondary" data-bs-theme="dark">
             <div className="container-fluid  row justify-content-center">
-                <section className="col-2 text-light">
+                <section className="col-2">
                     <div>
-                        <img src={me} width="200" height="200" className="d-inline-block align-text-top navImage"></img>
+                        <a href="/home">
+                            <img 
+                            src={me} 
+                            width="200" 
+                            height="200" 
+                            className="d-inline-block align-text-top navImage"
+                            >
+                            </img>
+                        </a>
                     </div>
                 </section>
                 <section className="col navbar-brand">
@@ -16,6 +25,17 @@ export default function Nav({links}) {
                     </div>
                 </section>
                 <section className="col-2">
+                    <div>
+                        <a href="/home">
+                            <img 
+                            src={nightstreak} 
+                            width="200" 
+                            height="200" 
+                            className="d-inline-block align-text-top navImage"
+                            >
+                            </img>
+                        </a>
+                    </div>
                 </section>
             </div>
         </nav>
