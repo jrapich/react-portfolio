690b8d069f861f1e5364a2420f2d278b4de8474f
jeremysr <jeremysr@protonmail.com>
2023-12-23 22:31:07 -0700

    added new color palette

--- portfolio/src/components/UI/footer/Footer.jsx
+++ portfolio/src/components/UI/footer/Footer.jsx
@@ -2,7 +2,7 @@ import './Footer.css';
 
 export default function Footer() {
     return(
-        <footer className='row container-fluid bg-body-secondary text-light d-block' data-bs-theme="dark">
+        <footer className='row container-fluid text-light d-block footer-colors'>
             <span>&copy; Jeremy Rapich 2023 </span>
             <span><a href='https://www.github.com/jrapich'>Github</a></span>
             <span><a href='https://www.linkedin.com/in/jeremy-rapich' >LinkedIn</a></span>
