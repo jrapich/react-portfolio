53f6021a0c4db0005413de6b7e98bef98f526e7d
Jeremy Rapich <jeremysr@protonmail.com>
2024-01-15 19:30:10 -0700

    added most recent resume and updated skills

--- portfolio/src/pages/resume/Resume.jsx
+++ portfolio/src/pages/resume/Resume.jsx
@@ -13,21 +13,25 @@ export default function Resume () {
                     <h3><b>SKILLS:</b></h3>
                     <div className="d-flex justify-content-start flex-column">
                         <ul>
-                            <li className="text-start">Proficient in writing and debugging Front-end and Back-end JavaScript;</li>
-                            <li className="text-start">Proficient in using HTML/CSS/JavaScript to build Full Stack Web Applications;</li>
-                            <li className="text-start">Experience in writing, debugging, and deploying RESTful API/MERN stack apps;</li>
-                            <li className="text-start">Experience in setting up and interfacing with MySQL and MongoDB databases;</li>
-                            <li className="text-start">Familiarity with ReactJS and GraphQL Apollo Client/Server;</li>
-                            <li className="text-start">Basic Experience in Ubuntu Server admin by self hosting and self managing a number of personal apps and projects;</li>
-                            <li className="text-start">Problem solving difficult situations while providing excellent customer service;</li>
+                            <li className="text-start">Writing and debugging Front-end and Back-end JavaScript;</li>
+                            <li className="text-start">Using HTML/CSS/JavaScript to build Full Stack Web Applications;</li>
+                            <li className="text-start">Building, debugging, and deploying RESTful API/MERN stack apps in a team environment;</li>
+                            <li className="text-start">NodeJS, ExpressJS, SequelizeJS, MongooseJS, HandlebarsJS, jQuery;</li>
+                            <li className="text-start">CSS/component libraries such as Bootstrap and Material UI;</li>
+                            <li className="text-start">Setting up and interfacing with MySQL and MongoDB databases;</li>
+                            <li className="text-start">ReactJS and GraphQL Apollo Client/Server;</li>
+                            <li className="text-start">Deploying apps, bare metal and via 3rd party(Heroku and Railway), AWS coming soon;</li>
+                            <li className="text-start">Ubuntu Server admin via terminal/self hosting personal apps and projects;</li>
+                            <li className="text-start">6+ years experience in problem solving difficult situations while providing excellent customer service(dispatch/communications);</li>
                             <li className="text-start">Typing speed of 50-60 WPM;</li>
+                            <li className="text-start">Always learning and improving! Python/Java/AngularJS/VueJS/React Native studying/learning coming soon;</li>
                         </ul>
                     </div>
                     <h3><b>EDUCATION AND CERTS:</b></h3>
                     <div className="d-flex justify-content-start flex-column">
                         <ul>
-                            <li className='text-start'>University of Utah Professional Education - FullStack WebDev Certificate -  Dec. 2023</li>
-                            <li className='text-start'>Tooele High School - HS Diploma - Mar. 2011</li>
+                            <li className='text-start'>Certificate - University of Utah Professional Education - Full Stack WebDev Bootcamp -  Dec. 2023</li>
+                            <li className='text-start'>HS Diploma - Tooele High School - Mar. 2011</li>
                         </ul>
                     </div>
                     <h3><b>INTERESTS:</b></h3>
@@ -41,7 +45,7 @@ export default function Resume () {
                     <h3><b>MILITARY SERVICE</b></h3>
                     <div className='d-flex justify-content-start flex-column'>
                         <ul>
-                            <li className="text-start">Apache Helicopter Mechanic - Utah Army National Guard - West Jordan, UT - Nov. 2011 to Nov. 2017</li>
+                            <li className="text-start">Apache Helicopter Mechanic - Secret Clearance - Utah Army National Guard - West Jordan, UT - Nov. 2011 to Nov. 2017</li>
                         </ul>
                     </div>
                     <h3><b>WORK EXPERIENCE</b></h3>
